747b340780094d9c771c5c8cc87b4a5f507d1169
Igor <criokondor@mail.ru>
2020-02-19 22:43:54 +0300

    Add bean cat with AOP befeore and after

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,8 @@
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import ru.test.spring.PetServise.Cat;
 import ru.test.spring.PetServise.PetService;
+import ru.test.spring.PetServise.Toy;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,14 +10,20 @@ public class Main {
                 new ClassPathXmlApplicationContext("AplicationContex.xml");
 
         PetService petService = applicationContext.getBean(PetService.class);
+        Cat cat = applicationContext.getBean(Cat.class);
         PetService petServiceCat = (PetService) applicationContext.getBean("PetServiceCat");
         PetService petServiceDog = (PetService) applicationContext.getBean("PetServiceDog");
-
+//default
         petService.createPet().getVoice();
+        //Явное присваивание кота
         petServiceCat.createPet().getVoice();
+        //Явное присваивание собаки
         petServiceDog.createPet().getVoice();
-        petServiceCat.createPet().getToy();
 
+        petServiceCat.createPet().getToy();
+        // aop before
+        cat.getToy();
+        // aop after
 
     }
 }
--- src/main/java/ru/test/spring/PetServise/Cat.java
+++ src/main/java/ru/test/spring/PetServise/Cat.java
@@ -2,7 +2,6 @@ package ru.test.spring.PetServise;
 
 public class Cat extends Pet{
 
-    private String nameCat;
     private Toy toy;
 
     public Cat(Toy toy) {
